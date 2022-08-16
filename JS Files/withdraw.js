e6dcf50e9840a239cd490ff2569fc343c4cc7fa0
aliftareq <aliftareq@gmail.com>
2022-08-16 15:13:18 +0600

    updated error handling

--- JS Files/withdraw.js
+++ JS Files/withdraw.js
@@ -5,24 +5,32 @@
 document.getElementById('btn-withdraw').addEventListener('click', function () {
     //step-2: get user input 
     const withdrawField = document.getElementById('withdraw-field');
-    const newWithdrawAmount = withdrawField.value;
+    const newWithdrawAmountString = withdrawField.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
 
     //Step-3 : get the current withdraw amount.
     const withdrawBox = document.getElementById('withdraw-box');
-    const currentWithdrawAmount = withdrawBox.innerText;
-
-    //Step-4 : set/update the value on withdraw amount.
-    const liveWithdrawAmount = parseFloat(currentWithdrawAmount) + parseFloat(newWithdrawAmount);
-    withdrawBox.innerText = liveWithdrawAmount;
+    const currentWithdrawAmountString = withdrawBox.innerText;
+    const currentWithdrawAmount = parseFloat(currentWithdrawAmountString);
 
     //S-5 : get the value of balance.
     const balanceBox = document.getElementById('balance-box');
-    const previousBalance = balanceBox.innerText;
+    const previousBalanceString = balanceBox.innerText;
+    const previousBalance = parseFloat(previousBalanceString);
+
+    // substep : Error handling
+    if (newWithdrawAmount > previousBalance) {
+        alert('You have not sufficient amount.')
+        return;
+    }
+
+    //Step-4 : set/update the value on withdraw amount.
+    const liveWithdrawAmount = currentWithdrawAmount + newWithdrawAmount;
+    withdrawBox.innerText = liveWithdrawAmount;
 
     //S-6 : set/update the value on balance.
-    const currentBalance = parseFloat(previousBalance) - parseFloat(newWithdrawAmount);
+    const currentBalance = previousBalance - newWithdrawAmount;
     balanceBox.innerText = currentBalance;
-
 
     // step-7: clear the input fields
     withdrawField.value = '';
